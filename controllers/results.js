8662fd66abc7b6ece2ca15b62235f57e72b40ef4
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-09-14 14:12:49 +0700

    added handle bars and replaced static html with data

--- controllers/results.js
+++ controllers/results.js
@@ -2,8 +2,56 @@
 const express = require('express')
 const router = express.Router()
 
+const data = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: ''
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      },
+      {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      }
+    ]
+  }
+]
+
 router.get('/', (req, res) => {
-  res.render('results')
+  res.render('results', { data: data })
 })
 // Create POST controller
 
