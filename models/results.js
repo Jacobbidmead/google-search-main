5622fa7e5dc2450fa0cf437c67e2dae8bdaa101b
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-09-16 14:35:17 +0700

    added schema

--- models/results.js
+++ models/results.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
+module.exports = mongoose.model('search-results', {
+  title: {
+    type: String,
+    required: true
+  },
+  description: String,
+  url: String,
+  links: [
+    {
+      title: String,
+      url: String
+    }
+  ]
+})
